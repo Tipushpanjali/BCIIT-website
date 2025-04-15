30bc5caa9dbfaba1e21e398cd7c0f680df04dbc9
tipushpanjali <tipushpa7@gmail.com>
2025-04-15 11:03:33 +0530

    new to old events

--- django-project-main/testapp/views.py
+++ django-project-main/testapp/views.py
@@ -3,7 +3,36 @@ from testapp.models import Faculty
 from testapp.models import Event, students, OldEvent
 from datetime import date
 
+
+def sync_events():
+    today = date.today()
+
+    past_events = Event.objects.filter(date__lt=today)
+    for event in past_events:
+        OldEvent.objects.create(
+            title=event.title,
+            date=event.date,
+            location=event.location,
+            description=event.description,
+            image=event.image
+        )
+        event.delete()
+
+    future_events = OldEvent.objects.filter(date__gte=today)
+    for old_event in future_events:
+        Event.objects.create(
+            title=old_event.title,
+            date=old_event.date,
+            location=old_event.location,
+            description=old_event.description,
+            image=old_event.image
+        )
+        old_event.delete()
+
+
 def home(request):
+    sync_events()
+  
     events = list(OldEvent.objects.all().order_by('date'))
     return render(request, 'testapp/home.html', {'events': events})
 
@@ -20,6 +49,7 @@ def faculty_list(request):
     return render(request, 'testapp/faculty.html', {'faculties': faculties})
 
 def events_list(request):
+    sync_events()
     events = list(Event.objects.all().order_by('date'))
 
     return render(request, 'testapp/events.html', {'events': events})
